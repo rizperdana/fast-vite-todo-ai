d0ec1a7b2e4f8135f1c07380bea5a7e64ec692fa
Rizki Perdana <perdana.rizki16@gmail.com>
2025-06-04 22:59:40 +0700

    add create component and endpoint

--- api/app/api.py
+++ api/app/api.py
@@ -22,16 +22,31 @@ async def read_root() -> dict:
     return {"message": "Welcome to todo list API!"}
 
 
+todos = [
+    {
+        "id": 1,
+        "item": "Read a book"
+    },
+    {
+        "id": 2,
+        "item": "Code project AZ"
+    }
+]
+
+
 @app.get("/todo", tags=["todos"])
 async def get_todos() -> dict:
-    todos = [
-        {
-            "id": 1,
-            "item": "Read a book"
-        },
-        {
-            "id": 2,
-            "item": "Code project AZ"
-        }
-    ]
     return { "data": todos }
+
+
+@app.post("/todo", tags=["todos"])
+async def add_todo(todo: dict) -> dict:
+    for t in todos:
+        if todo["id"] == t["id"]:
+            raise Exception("ID must unique")
+
+        if todo["item"].lower().strip() == t["item"].lower().strip():
+            raise Exception("Item cannot be the same")
+
+    todos.append(todo)
+    return {"data": {"Todo Added"}}
--- ui/src/components/Todos.tsx
+++ ui/src/components/Todos.tsx
@@ -1,5 +1,5 @@
-import { Container, Stack } from "@chakra-ui/react";
-import { createContext, useEffect, useState } from "react";
+import { Container, Input, Stack } from "@chakra-ui/react";
+import { ChangeEvent, createContext, FormEvent, useContext, useEffect, useState } from "react";
 
 
 interface Todo {
@@ -10,6 +10,41 @@ interface Todo {
 const TodosContext = createContext({
     todos: [], fetchTodos: () => {}
 })
+
+function AddTodo() {
+    const [item, setItem] = useState("")
+    const {todos, fetchTodos} = useContext(TodosContext)
+
+    const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+        setItem(event.target.value)
+    }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const newTodo = {
+            "id": todos.length + 1,
+            "item": item
+        }
+
+        fetch("http://localhost:7777/todo", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newTodo)
+        }).then(fetchTodos)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <Input
+                pr="4.5rem"
+                type="text"
+                placeholder="Add a todo item"
+                aria-label="Add a todo item"
+                onChange={handleInput}
+            />
+        </form>
+    )
+}
 
 export default function Todos() {
     const [todos, setTodos] = useState([])
@@ -25,6 +60,7 @@ export default function Todos() {
     return (
         <TodosContext.Provider value={{todos, fetchTodos}}>
             <Container maxW="container.x1" pt="100px">
+                <AddTodo />
                 <Stack gap={5}>
                     {todos.map((todo: Todo) => (
                         <b key={todo.id}>{todo.item}</b>
