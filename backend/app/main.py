7db79b108b984403468f865698cf7973eb944825
Rizki Perdana <perdana.rizki16@gmail.com>
2025-07-24 22:55:05 +0700

    add tortoise orm

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,47 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
+from tortoise import Tortoise, generate_config
+from tortoise.contrib.fastapi import RegisterTortoise, tortoise_exception_handlers
+
+
+@asynccontextmanager
+async def lifespan_test(app: FastAPI) -> AsyncGenerator[None, None]:
+    config = generate_config(
+        os.getenv("TORTOISE_TEST_DB", "sqlite://:memory:"),
+        app_modules={"models": ["models"]},
+        testing=True,
+        connection_label="models",
+    )
+    async with RegisterTortoise(
+        app=app,
+        config=config,
+        generate_schemas=True,
+        _create_db=True,
+    ):
+        # db connected
+        yield
+        # app teardown
+    # db connections closed
+    await Tortoise._drop_databases()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    if getattr(app.state, "testing", None):
+        async with lifespan_test(app) as _:
+            yield
+    else:
+        # app startup
+        async with register_orm(app):
+            # db connected
+            yield
+            # app teardown
+        # db connections closed
 
 app = FastAPI()
 
